4a5ec38613449c25963145e07c169aa070605130
탁세하 <takseha98@gmail.com>
2025-04-29 17:53:47 +0900

    style(#11): Notification 관련 변수명 수정

--- domain/src/main/java/com/takseha/domain/model/notification/Notification.kt
+++ domain/src/main/java/com/takseha/domain/model/notification/Notification.kt
@@ -3,7 +3,7 @@ package com.takseha.domain.model.notification
 data class Notification(
     val id: String,
     val studyInfoId: Int,
-    val createAt: String,
+    val createdAt: String,
     val message: String,
     val title: String
 )

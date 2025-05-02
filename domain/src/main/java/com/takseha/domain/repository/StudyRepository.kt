0089d8799ceaac426d19dd904de09713534b0f84
탁세하 <takseha98@gmail.com>
2025-05-02 23:02:29 +0900

    feat(#23): 필요없는 get 메소드 제거
    - 항상 서버에서 조회해야 하는 로직에 대한 get 메소드 제거

--- domain/src/main/java/com/takseha/domain/repository/StudyRepository.kt
+++ domain/src/main/java/com/takseha/domain/repository/StudyRepository.kt
@@ -6,15 +6,12 @@ import com.takseha.domain.model.study.StudySummary
 
 interface StudyRepository {
     /** 전체 스터디 리스트 조회 */
-    suspend fun getAllStudies(cursorIdx: Long?, limit: Long, sortBy: String): List<StudySummary>
     suspend fun fetchAllStudies(cursorIdx: Long?, limit: Long, sortBy: String): List<StudySummary>
 
     /** 전체 스터디 개수 조회 */
-    suspend fun getTotalStudyCount(): Int
     suspend fun fetchTotalStudyCount(): Int
 
     /** 스터디 랭킹 조회 */
-    suspend fun getStudyRank(studyId: Int): Int
     suspend fun fetchStudyRank(studyId: Int): Int
 
     /** 스터디 북마크 조회 */
@@ -22,7 +19,6 @@ interface StudyRepository {
     suspend fun fetchStudyBookmark(studyId: Int): Boolean
 
     /** 스터디 상세 정보 조회 */
-    suspend fun getStudyDetail(studyId: Int): StudyDetail
     suspend fun fetchStudyDetail(studyId: Int): StudyDetail
 
     /** 스터디 생성 */

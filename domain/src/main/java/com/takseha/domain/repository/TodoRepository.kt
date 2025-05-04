ba98d4dc270b0b068e99bcc571aac021d094d487
탁세하 <takseha98@gmail.com>
2025-05-04 12:59:05 +0900

    feat(#29): TodoRepository 정의

---
+++ domain/src/main/java/com/takseha/domain/repository/TodoRepository.kt
@@ -0,0 +1,23 @@
+package com.takseha.domain.repository
+
+import com.takseha.domain.model.notification.Notification
+import com.takseha.domain.model.todo.CreateTodoParam
+import com.takseha.domain.model.todo.TodoDetail
+
+interface TodoRepository {
+    /** 투두 리스트 조회 */
+    suspend fun fetchTodoList(cursorIdx: Long?, limit: Long, studyId: Int): List<Notification>
+
+    /** 투두 상세 정보 조회 */
+    suspend fun getTodoDetail(studyId: Int, todoId: Int): TodoDetail
+    suspend fun fetchTodoDetail(studyId: Int, todoId: Int): TodoDetail
+
+    /** 투두 등록 */
+    suspend fun createTodo(studyId: Int, createTodoParam: CreateTodoParam)
+
+    /** 투두 수정 */
+    suspend fun updateTodo(studyId: Int, todoId: Int, createTodoParam: CreateTodoParam)
+
+    /** 투두 삭제 */
+    suspend fun deleteTodo(studyId: Int, todoId: Int)
+}

00f80d5866e0e21204af794dffd1a86ca2e3cc62
탁세하 <takseha98@gmail.com>
2025-05-04 13:41:23 +0900

    feat(#31): CommitRepository 정의

---
+++ domain/src/main/java/com/takseha/domain/repository/CommitRepository.kt
@@ -0,0 +1,34 @@
+package com.takseha.domain.repository
+
+import com.takseha.domain.model.commit.CommitComment
+import com.takseha.domain.model.commit.CommitDetail
+import com.takseha.domain.model.commit.PostCommitCommentParam
+import com.takseha.domain.model.todo.CommitSummary
+
+interface CommitRepository {
+    /** 마이커밋 리스트 조회 */
+    suspend fun getMyCommits(cursorIdx: Long?, limit: Long, studyId: Int): List<CommitSummary>
+    suspend fun fetchMyCommits(cursorIdx: Long?, limit: Long, studyId: Int): List<CommitSummary>
+
+    /** 커밋 상세 정보 조회 */
+    suspend fun getCommitDetail(studyId: Int, commitId: Int): CommitDetail
+    suspend fun fetchCommitDetail(studyId: Int, commitId: Int): CommitDetail
+
+    /** 커밋 승인 */
+    suspend fun approveCommit(studyId: Int, commitId: Int)
+
+    /** 커밋 반려 */
+    suspend fun rejectCommit(studyId: Int, commitId: Int)
+
+    /** 커밋 댓글 리스트 조회 */
+    suspend fun fetchCommitComments(studyId: Int, commitId: Int): List<CommitComment>
+
+    /** 커밋 댓글 등록 */
+    suspend fun postCommitComment(commitId: Int, postCommitCommentParam: PostCommitCommentParam)
+
+    /** 커밋 댓글 수정 */
+    suspend fun updateCommitComment(commitId: Int, postCommitCommentParam: PostCommitCommentParam)
+
+    /** 커밋 댓글 삭제 */
+    suspend fun deleteCommitComment(commitId: Int, commentId: Int)
+}

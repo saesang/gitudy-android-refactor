55578e38db031ad7242ea238651e945302382799
탁세하 <takseha98@gmail.com>
2025-05-04 12:35:58 +0900

    feat(#27): NotificationRepository 정의

---
+++ domain/src/main/java/com/takseha/domain/repository/NotificationRepository.kt
@@ -0,0 +1,14 @@
+package com.takseha.domain.repository
+
+import com.takseha.domain.model.notification.Notification
+
+interface NotificationRepository {
+    /** 알림 리스트 조회 */
+    suspend fun fetchNotifications(cursorTime: String?, limit: Long): List<Notification>
+
+    /** 알림 삭제 */
+    suspend fun deleteNotification(notificationId: Int)
+
+    /** 전체 알림 삭제 */
+    suspend fun deleteAllNotifications()
+}

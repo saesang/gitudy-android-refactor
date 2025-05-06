698bdcb2e453487b32c1a6e7fe03229c50362ed9
탁세하 <takseha98@gmail.com>
2025-05-06 20:53:10 +0900

    feat(#71): RepositoryModule 구현

---
+++ core/src/main/java/com/takseha/core/di/RepositoryModule.kt
@@ -0,0 +1,39 @@
+package com.takseha.core.di
+
+import com.takseha.domain.repository.AuthRepository
+import com.takseha.domain.repository.CommitRepository
+import com.takseha.domain.repository.MyStudyRepository
+import com.takseha.domain.repository.NotificationRepository
+import com.takseha.domain.repository.ProfileRepository
+import com.takseha.domain.repository.StudyRepository
+import com.takseha.domain.repository.TodoRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+// TODO: 추후 주석 해제
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+//    @Binds
+//    abstract fun AuthRepository(repositoryImpl: AuthRepositoryImpl): AuthRepository
+//
+//    @Binds
+//    abstract fun CommitRepository(repositoryImpl: CommitRepositoryImpl): CommitRepository
+//
+//    @Binds
+//    abstract fun MyStudyRepository(repositoryImpl: MyStudyRepositoryImpl): MyStudyRepository
+//
+//    @Binds
+//    abstract fun NotificationRepository(repository: NotificationRepositoryImpl): NotificationRepository
+//
+//    @Binds
+//    abstract fun ProfileRepository(repository: ProfileRepositoryImpl): ProfileRepository
+//
+//    @Binds
+//    abstract fun StudyRepository(repositoryImpl: StudyRepositoryImpl): StudyRepository
+//
+//    @Binds
+//    abstract fun TodoRepository(repositoryImpl: TodoRepositoryImpl): TodoRepository
+}

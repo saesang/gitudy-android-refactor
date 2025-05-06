da3845f10458d15c325fb042fff519ceeb848c35
탁세하 <takseha98@gmail.com>
2025-05-06 19:12:35 +0900

    feat(#67): safeApiCall() 구현

---
+++ data/src/main/java/com/takseha/data/utils/safeApiCall.kt
@@ -0,0 +1,38 @@
+package com.takseha.data.utils
+
+import com.takseha.data.mapper.exception.ExceptionToFailureMapper
+import com.takseha.domain.model.common.Failure
+import retrofit2.Response
+
+suspend fun <T, R> safeApiCall(
+    call: suspend () -> Response<T>,
+    mapper: (T) -> R,
+    successApi: (suspend (T) -> Unit)? = null
+): R {
+    return try {
+        val response = call.invoke()
+        val body = response.body()
+
+        when {
+            !response.isSuccessful -> {   // response.code != 200
+                val statusCode = response.code()
+                val errorDesc = "HTTP Error($statusCode)"
+
+                throw Failure.ServerFailure(Exception(errorDesc))
+            }
+
+            body == null -> {   // null 반환
+                throw Failure.ServerFailure(Exception("null 반환"))
+            }
+
+            else -> {
+                successApi?.let { apiCall ->
+                    apiCall(body)
+                }
+                mapper(body)
+            }
+        }
+    } catch (e: Exception) {
+        throw ExceptionToFailureMapper.map(e)   // 에러 발생 시 자동으로 Failure로 래핑해서 throw
+    }
+}
